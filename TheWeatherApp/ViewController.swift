5f1a56abf0ffcc827ad356d359a85d379267fe30
k6unal <61171171+k6unal@users.noreply.github.com>
2020-07-16 14:11:10 +1000

    Initial Commit

---
+++ TheWeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheWeatherApp
+//
+//  Created by KUNAL PARIKH on 16/7/20.
+//  Copyright © 2020 KUNAL PARIKH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
